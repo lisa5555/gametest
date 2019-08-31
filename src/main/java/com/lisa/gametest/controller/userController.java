5ddd8123e7fc96420d81f33edc67dcd9430e5368
renr <74255@aliyun.com>
2019-08-31 10:27:11 +0800

    动态获取菜单

--- src/main/java/com/lisa/gametest/controller/userController.java
+++ src/main/java/com/lisa/gametest/controller/userController.java
@@ -1,21 +1,30 @@
 package com.lisa.gametest.controller;
 
 import com.lisa.gametest.common.AjaxResult;
+import com.lisa.gametest.entity.TUser;
+import com.lisa.gametest.service.ITUserService;
 import org.apache.shiro.SecurityUtils;
 import org.apache.shiro.authc.AuthenticationException;
 import org.apache.shiro.authc.UsernamePasswordToken;
 import org.apache.shiro.subject.Subject;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpSession;
 
 @Controller
 @ResponseBody
 @RequestMapping("/user")
 public class userController {
+    @Autowired
+    private ITUserService service;
     @RequestMapping("/login.do")
-    public AjaxResult login(String username,String password){
+    public AjaxResult login(String username, String password, HttpSession session){
         UsernamePasswordToken token = new UsernamePasswordToken(username, password);
+        TUser user = service.findByName(username);
+        session.setAttribute("user",user);
         // 获取Subject主体对象
         Subject subject = SecurityUtils.getSubject();
         AjaxResult result = new AjaxResult();
